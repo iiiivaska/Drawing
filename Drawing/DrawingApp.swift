a598515c79e8bc9f661e12159c02aadbead5c1e3
iiiivaska <iiiivaska@yandex.ru>
2021-11-04 13:37:46 +0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Василий Буланов on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
